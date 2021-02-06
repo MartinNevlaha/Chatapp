03cb5d98254cebbb1a20c8e3d22aaf07265651b6
MartinNevlaha <martin.nevlaha@gmail.com>
2021-02-06 18:02:20 +0100

    setup basic midlewares

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`)
