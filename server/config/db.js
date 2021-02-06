8e3e7287d10f8d4721d46d5de43e65519f525d98
MartinNevlaha <martin.nevlaha@gmail.com>
2021-02-06 18:58:14 +0100

    db logging based on NODE_ENV variable

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 const { Sequelize } = require("sequelize");
+const timestamp = require("time-stamp");
+
+const logger = require("./winston");
 
 const DATABASE = process.env.DATABASE;
 const DB_USERNAME = process.env.DB_USERNAME;
@@ -8,13 +11,16 @@ const DB_HOST = process.env.DB_HOST;
 const sequelize = new Sequelize(DATABASE, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "postgres",
-  logging: () => {
-    if (process.env.NODE_ENV === "production") {
-      return false;
-    } else {
-      return true;
-    }
-  }
+  logging:
+    process.env.NODE_ENV === "production"
+      ? false
+      : (msg) => {
+          logger.debug({
+            time: timestamp("YYYY/MM/DD/HH:mm:ss"),
+            level: "debug",
+            message: msg,
+          });
+        },
 });
 
 module.exports = sequelize;
