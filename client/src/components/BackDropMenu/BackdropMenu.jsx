176f3567b01660763a0fa4195b6e0a5363c154f9
MartinNevlaha <martin.nevlaha@gmail.com>
2021-02-07 20:47:31 +0100

    styled popover menu

--- client/src/components/BackDropMenu/BackdropMenu.jsx
+++ client/src/components/BackDropMenu/BackdropMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Flags from 'country-flag-icons/react/1x1';
+import Flags from "country-flag-icons/react/1x1";
 
 import classes from "./BackDropMenu.module.scss";
 
@@ -8,8 +8,18 @@ const BackDropMenu = ({ openMenu, handleClose }) => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      
+    <div
+      className={
+        Boolean(openMenu)
+          ? [classes.backDropMenu, classes.open].join(" ")
+          : classes.backDropMenu
+      }
+    >
+      <ul className={classes.backDropMenu_items}>
+        <li className={classes.backDropMenu_item}>Profile</li>
+        <li className={classes.backDropMenu_item}>My Account</li>
+        <li className={classes.backDropMenu_item}>Logout</li>
+      </ul>
     </div>
   );
 };
