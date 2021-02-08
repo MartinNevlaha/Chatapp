8fcec08ae592ed1d888330bdbf9ee2fcde6acfd3
Martin Nevlaha <martin.nevlaha@nov.sk>
2021-02-08 15:16:11 +0100

    connect redux to app

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
 import reportWebVitals from './reportWebVitals';
 
 import "./i18n/i18n";
 
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers({
+
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <React.Fragment>
-    <App />
-    
+  <Provider store={store}>
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+  </Provider>
   </React.Fragment>,
   document.getElementById('root')
 );
