51fa10b210cf08b9f90c4b44d95342cbd82fa32a
Martin Nevlaha <martin.nevlaha@nov.sk>
2021-02-08 10:52:02 +0100

    finished language select options

--- client/src/components/BackDropMenu/BackdropMenu.jsx
+++ client/src/components/BackDropMenu/BackdropMenu.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useRef, useEffect} from "react";
 import { useTranslation } from "react-i18next";
+import i18next from 'i18next';
 import Flags from "country-flag-icons/react/1x1";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
@@ -29,6 +30,13 @@ const BackDropMenu = (props) => {
     setOpenMenu(event.currentTarget);
   };
 
+  const handleLanguageChange = (language) => {
+    i18next.changeLanguage(language, (err) => {
+      if (err) return console.log("Something went wrong", err)
+    })
+    setOpenMenu(null);
+  }
+
   return (
     <React.Fragment>
     <div className={classes.account_icon_wrapper} onClick={handleClick}>
@@ -50,12 +58,13 @@ const BackDropMenu = (props) => {
           <li>Profile</li>
           <li>My Account</li>
           <h3>Vyber svoj jazyk</h3>
-          <li>            <div className={classes.language_wrapper}>
+          <li onClick={() => handleLanguageChange("en")}>            
+          <div className={classes.language_wrapper}>
               <Flags.GB title="Anglicky" className={classes.language_wrapper_flags}/>Anglicky
             </div></li>
-          <li>
+          <li onClick={() => handleLanguageChange("sk")}>
             <div className={classes.language_wrapper}>
-              <Flags.SK title="Slovak" className={classes.language_wrapper_flags}/>Slovenský
+              <Flags.SK title="Slovak" className={classes.language_wrapper_flags}/>Slovensky
             </div>
           </li>
           <li>Logout</li>
