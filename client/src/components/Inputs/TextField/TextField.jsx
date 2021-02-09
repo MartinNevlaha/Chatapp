8a91d7d9b8bb196709bdf42c10966b3a0036dc14
MartinNevlaha <martin.nevlaha@gmail.com>
2021-02-09 20:38:59 +0100

    show / hide pwd

--- client/src/components/Inputs/TextField/TextField.jsx
+++ client/src/components/Inputs/TextField/TextField.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
 import { useField, ErrorMessage } from "formik";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./TextField.module.scss";
 
-const TextField = ({label, ...props}) => {
-    const [field, meta] = useField(props);
-    return (
-        <div className={classes.textField}>
-            <div className={classes.textField_wrapper}>
-            <label htmlFor={field.name}>{label}</label>
-            <input className={classes.textField_input} 
-            autoComplete="off" 
-            placeholder={field.name}
-            {...field} 
-            {...props}/>
-            <ErrorMessage component="div" style={{color:"red", fontSize: ".8rem", padding: ".5rem"}} name={field.name}/>
-            </div>
-        </div>
-    )
-}
+const TextField = ({ label, type, ...props }) => {
+  const [field, meta] = useField(props);
+  const [isShow, setIsShow] = useState(false);
 
-export default TextField
+  const handleTogleShowPwd = () => {
+    setIsShow(!isShow);
+  };
+
+  return (
+    <div className={classes.textField}>
+      <div className={classes.textField_wrapper}>
+        <label htmlFor={field.name}>{label}</label>
+        <input
+          className={classes.textField_input}
+          autoComplete="off"
+          placeholder={field.name}
+          type={field.name && isShow  ? "text" : type}
+          {...field}
+          {...props}
+        />
+        {field.name === "password" && (
+          <FontAwesomeIcon
+            icon={isShow ? faEyeSlash : faEye}
+            className={classes.icon}
+            cursor="pointer"
+            onClick={handleTogleShowPwd}
+          />
+        )}
+
+        <ErrorMessage
+          component="div"
+          style={{ color: "red", fontSize: ".8rem", padding: ".5rem" }}
+          name={field.name}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TextField;
