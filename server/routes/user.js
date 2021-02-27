59f30829c842bf2a8f457934aff0f7168b2b7510
MartinNevlaha <martin.nevlaha@gmail.com>
2021-02-27 15:31:51 +0100

    validation

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const isAuth = require("../middleware/isAuth");
+const { validateResults } = require("../validators/");
+const { rules: updateRules } = require("../validators/user/update");
 const { userUpdate } = require("../controllers/user");
 
-router.put("/update", isAuth, userUpdate);
+router.put("/update", [isAuth, updateRules, validateResults], userUpdate);
 
 module.exports = router;
