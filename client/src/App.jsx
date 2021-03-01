7d3c51cac5dc0ef9518ac8f6db94f3267bf3405f
MartinNevlaha <martin.nevlaha@gmail.com>
2021-03-01 17:54:05 +0100

    refactor router to lazy loading components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense, lazy } from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
@@ -8,33 +8,38 @@ import Layout from "./HOC/Layout";
 import Toast from "./components/UI/Toast/Toast";
 import ProtectedRoute from "./Router/ProtectedRoute";
 import * as action from "./store/actions";
+import Spinner from "./components/UI/Spinner/Spinner";
 
-import EmailActivation from "./containers/EmailActivation";
-import Login from "./containers/Login";
-import Chat from "./containers/Chat";
-import UserUpdate from "./containers/UserUpdate";
-
-
-let routes = (
-  <Switch>
-    <ProtectedRoute path="/update-profile" component={UserUpdate} />
-    <ProtectedRoute path="/" exact component={Chat} />
-    <Route path="/activation/:token" component={EmailActivation} />
-    <Route path="/login" component={Login} /> 
-    <Redirect to="/" />
-  </Switch>
-);
+//component lazy load
+const Login = lazy(() => import("./containers/Login"));
+const Chat = lazy(() => import("./containers/Chat"));
+const UserUpdate = lazy(() => import("./containers/UserUpdate"));
+const EmailActivation = lazy(() => import("./containers/EmailActivation"));
 
 function App() {
   const dispatch = useDispatch();
   const error = useSelector((state) => state.requestStatus.error);
   const success = useSelector((state) => state.requestStatus.success);
+  const isAuth = useSelector((state) => state.userAuth.token);
 
   useEffect(() => {
     dispatch(action.authCheckState());
   }, [dispatch]);
 
   const { t } = useTranslation();
+
+  let routes = (
+    <Suspense fallback={Spinner}>
+      <Switch>
+        <ProtectedRoute path="/update-profile" component={UserUpdate} />
+        <ProtectedRoute path="/" exact component={Chat} />
+        <Route path="/activation/:token" component={EmailActivation} />
+        {isAuth ? null : <Route path="/login" component={Login} />}
+        <Redirect to="/" />
+      </Switch>
+    </Suspense>
+  );
+
   return (
     <div className="App">
       <Layout>
