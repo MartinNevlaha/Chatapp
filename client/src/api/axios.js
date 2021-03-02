816245e4e1caea8103e1c8f319db1d73e0332034
MartinNevlaha <martin.nevlaha@gmail.com>
2021-03-02 19:07:33 +0100

    fix request axios interceptor

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 
+import store from "../store";
+
 const instance = axios.create({
   baseURL: "http://localhost:8000",
   headers: {
     "Accept": "application/json",
-    'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
   },
 });
+
+instance.interceptors.request.use((config) => {
+  const token = store.getState().userAuth.token;
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+})
 
 export default instance;
