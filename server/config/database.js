e1ddf02f03ebc84e709894efb7a37b6e4bf01703
MartinNevlaha <martin.nevlaha@gmail.com>
2021-03-19 18:32:23 +0100

    temporary disable default db logging

--- server/config/database.js
+++ server/config/database.js
@@ -7,6 +7,7 @@ module.exports = {
     database: process.env.DATABASE,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    logging: false
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -14,6 +15,7 @@ module.exports = {
     database: process.env.DATABASE,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    logging: false
   },
   production: {
     username: process.env.DB_USERNAME,
@@ -21,5 +23,6 @@ module.exports = {
     database: process.env.DATABASE,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    logging: false
   },
 };
