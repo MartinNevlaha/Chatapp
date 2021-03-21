a20f75f9bafba38b25e9a4a53ba0098760c2ff52
MartinNevlaha <martin.nevlaha@gmail.com>
2021-03-21 19:17:47 +0100

    all users with status

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,6 @@
-const User = require("../models/").User;
+const { Op } = require("sequelize");
+const models = require("../models");
+const { User, Friendship } = models;
 
 exports.getUsers = async (req, res, next) => {
   const limit = parseInt(req.query.limit);
@@ -8,26 +10,32 @@ exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.findAndCountAll({
       where: {
-        activated: true
+        [Op.not]: { id: req.user.id },
+        activated: true,
       },
       attributes: {
-        exclude: ["password", "activationToken", "activated", "email"]
+        exclude: ["password", "activationToken", "activated", "email"],
+      },
+      include: {
+        model: Friendship,
+        //dorob reversne priatelstvo
       },
       limit: limit,
       offset: offset,
-      order: [["id", "ASC"]]
+      order: [["id", "ASC"]],
     });
     if (!users) {
       const error = new Error("Cant find any active user");
       error.statusCode = 404;
       return next(error);
     }
+
     res.json({
       status: "ok",
       message: "Users successfully loaded",
       count: users.count,
-      users: users.rows
-    })
+      users: users.rows,
+    });
   } catch (error) {
     if (error.statusCode) {
       error.statusCode = 500;
