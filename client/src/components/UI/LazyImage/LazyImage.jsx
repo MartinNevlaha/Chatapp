b5f60a4f2f26159fff3e9d4c2296f5adf1e36138
MartinNevlaha <martin.nevlaha@gmail.com>
2021-03-28 14:16:59 +0200

    delete scss file

--- client/src/components/UI/LazyImage/LazyImage.jsx
+++ client/src/components/UI/LazyImage/LazyImage.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
-import classes from "./LazyImage.module.scss";
 
 export const LazyImage = ({ image }) => {
   return (
-    <div className={classes.image_container}>
+    <div>
       <LazyLoadImage alt={image.alt} src={image.src} effect="opacity" />
     </div>
   );
