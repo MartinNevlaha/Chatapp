503644e674b19b7c648267e45da4fdd04d68f929
MartinNevlaha <martin.nevlaha@gmail.com>
2021-04-14 17:17:53 +0200

    added message and user association

--- server/models/user.js
+++ server/models/user.js
@@ -17,7 +17,8 @@ module.exports = (sequelize, DataTypes) => {
       this.hasMany(models.Post, { foreignKey: "userId" });
       this.hasMany(models.Likes, {foreignKey: "userId"});
       this.belongsToMany(models.Chat, {through: "ChatUser", foreignKey: "userId"});
-      this.hasMany(models.ChatUser, {foreignKey: "userId"})
+      this.hasMany(models.ChatUser, {foreignKey: "userId"});
+      this.hasMany(models.Message, {foreignKey: "fromUserId"});
     }
   }
   User.init(
