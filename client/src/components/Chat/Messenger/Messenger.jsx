0bd6dc548d601160d87693cb140a95de348778a7
MartinNevlaha <martin.nevlaha@gmail.com>
2021-04-15 19:54:11 +0200

    prepare header for messenger

--- client/src/components/Chat/Messenger/Messenger.jsx
+++ client/src/components/Chat/Messenger/Messenger.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 import classes from "./Messenger.module.scss";
+import SearchInput from "../../Inputs/SearchInputs/SearchInputs";
 
 const Messenger = () => {
-  return <div className={classes.messenger}>
-    <div className={classes.messenger_header}>
-
+  return (
+    <div className={classes.messenger}>
+      <div className={classes.messenger_header}>
+        <h2>Messenger</h2>
+        <div className={classes.messenger_header_input}>
+          <SearchInput styleType="large" />
+        </div>
+      </div>
+      <div className={classes.messenger_chats}></div>
     </div>
-    <div className={classes.messenger_chats}>
-
-    </div>
-  </div>;
+  );
 };
 
 export default Messenger;
