e7de3d9daf138b2301defd008e4a1107959b2e70
MartinNevlaha <martin.nevlaha@gmail.com>
2021-05-08 12:17:58 +0200

    translation and refactor NavItem

--- client/src/components/Menu/NavItem/NavItem.jsx
+++ client/src/components/Menu/NavItem/NavItem.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
 
 import classes from "./NavItem.module.scss";
 
-const NavItem = (props) => {
+const NavItem = ({ icon, link, exact, close, children }) => {
+  NavItem.propTypes = {
+    icon: PropTypes.object,
+    link: PropTypes.string,
+    exact: PropTypes.bool,
+    close: PropTypes.func,
+    children: PropTypes.node
+  };
   return (
     <li className={classes.navItem}>
       <FontAwesomeIcon
-        icon={props.icon}
+        icon={icon}
         color="rgb(80, 79, 79)"
         className={classes.navItem_icon}
       />
       <NavLink
         className={classes.navItem_a}
-        to={props.link}
-        exact={props.exact}
+        to={link}
+        exact={exact}
         activeClassName={classes.active}
-        onClick={props.close}
+        onClick={close}
       >
-        {props.children}
+        {children}
       </NavLink>
     </li>
   );
