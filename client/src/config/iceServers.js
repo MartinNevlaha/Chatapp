207c84806d50402b7b09065c11bbc9192097a1b0
MartinNevlaha <martin.nevlaha@gmail.com>
2021-05-14 19:04:43 +0200

    add turn server

--- client/src/config/iceServers.js
+++ client/src/config/iceServers.js
@@ -1,4 +1,8 @@
 export const ICE_SERVERS = [
-  { urls: process.env.REACT_APP_ICE_SERVER_1 },
-  { urls: process.env.REACT_APP_ICE_SERVER_2 },
+  { urls: process.env.REACT_APP_STUN_SERVER },
+  {
+    urls: process.env.REACT_APP_TURN_SERVER,
+    username: process.env.REACT_APP_TURN_SERVER_USERNAME,
+    credential: process.env.REACT_APP_TURN_SERVER_PASSWORD,
+  },
 ];
