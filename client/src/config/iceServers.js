321c26cb087a28ae414a9537115b18895e2e16ab
MartinNevlaha <martin.nevlaha@gmail.com>
2021-05-16 10:42:10 +0200

    fix wrong config

--- client/src/config/iceServers.js
+++ client/src/config/iceServers.js
@@ -1,6 +1,6 @@
 export const ICE_SERVERS = [
   { urls: process.env.REACT_APP_STUN_SERVER_1 },
-  { urls: process.env.REACT_APP_TURN_SERVER_2 },
+  { urls: process.env.REACT_APP_STUN_SERVER_2 },
   {
     urls: process.env.REACT_APP_TURN_SERVER,
     username: process.env.REACT_APP_TURN_SERVER_USERNAME,
