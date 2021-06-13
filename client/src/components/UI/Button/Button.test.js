23eb261e5aa4464968b514a2f5cef4b1d715cdc8
MartinNevlaha <martin.nevlaha@gmail.com>
2021-06-13 14:18:31 +0200

    basic setup of component test

---
+++ client/src/components/UI/Button/Button.test.js
@@ -0,0 +1,22 @@
+import { shallow } from "enzyme";
+
+import { findByTestAttr, checkProps } from "../../../test/testUtils";
+import Button from "./Button";
+
+const defaultProps = {
+  disabled: false,
+  children: "Ok",
+  clicked: jest.fn(),
+};
+
+/**
+ * Basic func to create a ShallowWrapper for the Button component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = () => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Button {...setupProps} />);
+};
