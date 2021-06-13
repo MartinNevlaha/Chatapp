f18c2213b78be6357e7b3f799ee09561f414f80f
MartinNevlaha <martin.nevlaha@gmail.com>
2021-06-13 15:30:08 +0200

    click on btn test

--- client/src/components/UI/Button/Button.test.js
+++ client/src/components/UI/Button/Button.test.js
@@ -1,11 +1,11 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 
 import { findByTestAttr, checkProps } from "../../../test/testUtils";
 import Button from "./Button";
 
 const defaultProps = {
   children: "Ok",
-  clicked: jest.fn(),
+  clicked: () => {},
 };
 
 /**
@@ -17,13 +17,103 @@ const defaultProps = {
 
 const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
-  return shallow(<Button {...setupProps} />);
+  return mount(<Button {...setupProps} />);
 };
 
 test("renders without error", () => {
   const wrapper = setup();
   const btn = findByTestAttr(wrapper, "component-button");
   expect(btn.length).toBe(1);
+});
+
+describe("Test onClick btn function", () => {
+  const mockClick = jest.fn();
+  test("click btn when disabled prop is not provided", () => {
+    const wrapper = setup({ clicked: mockClick });
+    const btn = findByTestAttr(wrapper, "component-button");
+    btn.simulate("click");
+    expect(mockClick).toHaveBeenCalledTimes(1);
+  });
+  
+  test("click btn when disabled is false", () => {
+    const wrapper = setup({ clicked: mockClick, disabled: false });
+    const btn = findByTestAttr(wrapper, "component-button");
+    btn.simulate("click");
+    expect(mockClick).toHaveBeenCalledTimes(1);
+  });
+
+  test("click btn when disabled is true", () => {
+    const wrapper = setup({ clicked: mockClick, disabled: true });
+    const btn = findByTestAttr(wrapper, "component-button");
+    btn.simulate("click");
+    expect(mockClick).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe("test css class based on props", () => {
+  test("Button component has main class btn", () => {
+    const wrapper = setup();
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("btn")).toEqual(true);
+  });
+
+  test("Button component has not class disabled if disabled prop is not provided", () => {
+    const wrapper = setup();
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("disabled")).toEqual(false);
+  });
+
+  test("Button component has class disabled if disabled prop is true", () => {
+    const wrapper = setup({
+      disabled: true,
+    });
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("disabled")).toEqual(true);
+  });
+
+  test("Button component has not class disabled if disabled prop is false", () => {
+    const wrapper = setup({
+      disabled: false,
+    });
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("disabled")).toEqual(false);
+  });
+
+  test("Button component has class disabled when danger prop is true and disabled is true", () => {
+    const wrapper = setup({
+      disabled: true,
+      danger: true,
+    });
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("disabled")).toEqual(true);
+  });
+
+  test("Button component has not class danger when danger prop is true and disabled is true", () => {
+    const wrapper = setup({
+      disabled: true,
+      danger: true,
+    });
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("danger")).toEqual(false);
+  });
+
+  test("Button component has class danger when danger prop is true and disabled is false", () => {
+    const wrapper = setup({
+      disabled: false,
+      danger: true,
+    });
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("danger")).toEqual(true);
+  });
+
+  test("Button component has not class disabled when danger prop is true and disabled is false", () => {
+    const wrapper = setup({
+      disabled: false,
+      danger: true,
+    });
+    const btn = findByTestAttr(wrapper, "component-button");
+    expect(btn.hasClass("disabled")).toEqual(false);
+  });
 });
 
 describe("prop types checking", () => {
@@ -35,10 +125,10 @@ describe("prop types checking", () => {
     const optionalProps = {
       type: "submit",
       disabled: true,
-      clicked: jest.fn(),
+      clicked: () => {},
       children: "Ok",
       danger: false,
     };
-    checkProps(Button, optionalProps)
+    checkProps(Button, optionalProps);
   });
 });
