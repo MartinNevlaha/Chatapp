17964a8caf0b3b93d7a749b9bc8aa78f49d522e3
MartinNevlaha <martin.nevlaha@gmail.com>
2021-06-13 14:21:57 +0200

    test - renders without error

--- client/src/components/UI/Button/Button.test.js
+++ client/src/components/UI/Button/Button.test.js
@@ -16,7 +16,13 @@ const defaultProps = {
  * @returns {ShallowWrapper}
  */
 
-const setup = () => {
+const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
   return shallow(<Button {...setupProps} />);
 };
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const btn = findByTestAttr(wrapper, "component-button");
+  expect(btn.lenght).toBe(1);
+})
