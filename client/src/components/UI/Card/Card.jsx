7deca73c381f97c917033f5d06164c6c7ea6dfdd
MartinNevlaha <martin.nevlaha@gmail.com>
2021-06-15 06:44:07 +0200

    completed test

--- client/src/components/UI/Card/Card.jsx
+++ client/src/components/UI/Card/Card.jsx
@@ -3,17 +3,23 @@ import PropTypes from "prop-types";
 
 import classes from "./Card.module.scss";
 
-const Card = ({type, children}) => {
-
-  Card.propTypes = {
-    type: PropTypes.string,
-  };
-
+const Card = ({ type, children }) => {
   return (
-    <div className={[classes.card_wrapper, classes[type]].join(" ")}>
+    <div
+      className={[classes.card_wrapper, classes[type]].join(" ")}
+      data-test="component-card"
+    >
       {children}
     </div>
   );
+};
+
+Card.propTypes = {
+  type: PropTypes.oneOf(["small_card", "medium_card", "wide_angle"]),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
 };
 
 export default Card;
--- client/src/components/UI/Card/Card.test.js
+++ client/src/components/UI/Card/Card.test.js
@@ -4,11 +4,47 @@ import { checkProps, findByTestAttr } from "../../../test/testUtils";
 import Card from "./Card";
 
 const defaultProps = {
-  type: "small-card",
   children: <p>render something</p>,
 };
 
 const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
-  return shallow(<Card {...props} />);
+  return shallow(<Card {...setupProps} />);
 };
+
+test("should renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-card");
+  expect(component.length).toBe(1);
+});
+
+test("should renders children prop", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-card");
+  expect(component.containsMatchingElement(<p>render something</p>)).toEqual(
+    true
+  );
+});
+
+describe("css class check", () => {
+  test("basic css class", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, "component-card");
+    expect(component.hasClass("card_wrapper")).toEqual(true);
+  });
+
+  test("when type props is small_card element has css class small_card", () => {
+    const wrapper = setup({ type: "small_card" });
+    const component = findByTestAttr(wrapper, "component-card");
+    expect(component.hasClass("small_card")).toEqual(true);
+  });
+});
+
+describe("check propTypes", () => {
+  test("check default propTypes", () => {
+    checkProps(Card, defaultProps);
+  });
+  test("check optional propTypes", () => {
+    checkProps(Card, { type: "small_card", children: <p>render something</p> });
+  });
+});
