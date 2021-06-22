9de55f53f170811c5faae8170f28df831aefcdd0
MartinNevlaha <martin.nevlaha@gmail.com>
2021-06-22 20:18:18 +0200

    create storeFactory function

--- client/src/test/testUtils.js
+++ client/src/test/testUtils.js
@@ -1,4 +1,12 @@
 import checkPropTypes from "check-prop-types";
+import { createStore, applyMiddleware } from "redux";
+
+import rootReducer from "../store/reducers";
+import { midlewares } from "../store";
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState, applyMiddleware(...midlewares));
+}
 
 export const findByTestAttr = (wrapper, value) => {
   return wrapper.find(`[data-test='${value}']`);
